74245713388dab85a7f701de14f3525932c34e44
Paul MADELINE <paul.madeline@soprasteria.com>
2021-11-05 15:07:34 +0100

    Create The ItemController

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+using WishList.Models;
+
+namespace WishList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        ItemController(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        // GET: ItemController
+        public IActionResult Index()
+        {
+            return View("Index", _context.Items);
+        }
+
+        // GET: ItemController/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        // POST: ItemController/Create
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: ItemController/Delete/5
+        public IActionResult Delete(int Id)
+        {
+            Item item = _context.Items.SingleOrDefault(x => x.Id == Id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
